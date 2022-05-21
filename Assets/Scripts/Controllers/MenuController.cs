0a17450728467459291713056ed2ab37709319e7
Bastian Neubecker <bastian.neubecker@gmx.de>
2022-05-21 23:31:01 +0200

    CreatedMenuScene

---
+++ Assets/Scripts/Controllers/MenuController.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject MenuItems;
+    public GameObject CreateGameItems;
+    public GameObject JoinGameItems;
+	public GameObject OptionItems;
+	public GameObject BackOption;
+
+    public void onHostGameButtonPressed()
+	{
+        disableMenuObjects();
+        enableCreateLobbyObjects();
+	}
+
+    public void onJoinGameButtonPressed()
+	{
+        disableMenuObjects();
+        enableJoinLobbyObjects();
+	}
+
+    public void onOptionButtonPressed()
+	{
+		disableMenuObjects();
+		enableOptionObjects();
+	}
+
+	public void onBackButtonPressed()
+	{
+		disableEveryOtherObjects();
+		enableMenuObjects();
+	}
+
+	private void enableMenuObjects()
+	{
+		MenuItems.SetActive(true);
+		BackOption.SetActive(false);
+	}
+
+	private void disableEveryOtherObjects()
+	{
+		CreateGameItems.SetActive(false);
+		JoinGameItems.SetActive(false);
+		OptionItems.SetActive(false);
+	}
+
+	private void disableMenuObjects()
+	{
+        MenuItems.SetActive(false);
+	}
+
+	private void enableCreateLobbyObjects()
+	{
+        CreateGameItems.SetActive(true);
+		BackOption.SetActive(true);
+    }
+
+	private void enableJoinLobbyObjects()
+	{
+        JoinGameItems.SetActive(true);
+		BackOption.SetActive(true);
+	}
+
+	private void enableOptionObjects()
+	{
+		OptionItems.SetActive(true);
+		BackOption.SetActive(true);
+	}
+}
